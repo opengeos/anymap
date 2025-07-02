44734d3ba268ad71fc40d584e14ae0ab00ad2858
Qiusheng Wu <giswqs@gmail.com>
2025-07-02 01:35:00 -0400

    Add support for potree mapping backend

--- anymap/__init__.py
+++ anymap/__init__.py
@@ -4,6 +4,6 @@ __author__ = """Qiusheng Wu"""
 __email__ = "giswqs@gmail.com"
 __version__ = "0.1.0"
 
-from .anymap import MapWidget, MapLibreMap, MapboxMap, CesiumMap
+from .anymap import MapWidget, MapLibreMap, MapboxMap, CesiumMap, PotreeMap
 
-__all__ = ["MapWidget", "MapLibreMap", "MapboxMap", "CesiumMap"]
+__all__ = ["MapWidget", "MapLibreMap", "MapboxMap", "CesiumMap", "PotreeMap"]
