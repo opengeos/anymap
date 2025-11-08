225efa678c05cc59f688edc21a3b8a795594a849
Qiusheng Wu <giswqs@gmail.com>
2025-11-07 21:17:27 -0500

    Fix pytest error

--- anymap/__init__.py
+++ anymap/__init__.py
@@ -5,7 +5,7 @@ __email__ = "giswqs@gmail.com"
 __version__ = "0.8.0"
 
 from .base import MapWidget
-from .maplibre import MapLibreMap as Map
+from .maplibre import MapLibreMap
 from .mapbox import MapboxMap
 from .cesium import CesiumMap
 from .potree import PotreeMap
@@ -17,8 +17,11 @@ from .compare import MapCompare
 
 from .utils import download_file
 
+Map = MapLibreMap
+
 __all__ = [
     "Map",
+    "MapLibreMap",
     "MapWidget",
     "MapboxMap",
     "CesiumMap",
